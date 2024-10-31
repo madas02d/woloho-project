b148f2e6c0914243b7baf5efa8090c02fd84291d
madas02d <mb.damascene24@gmail.com>
2024-10-31 14:10:16 +0100

    first update

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <>
+        <header className="main-header">
+        <div className="header-logo p-2 ml-12"><h1>Woholo</h1></div>
+        <nav>
+            <ul className="Nav-items flex space-x-4 p-6 mr-8">
+                <li>Creat an ad</li>
+                <li>Letter Recived</li>
+                <li>Our Team</li>
+                <li>FAQ</li>
+            </ul>
+     </nav>
+        </header>
+        </>
+    )
+}
+export default Header
